10b0af3c1c5563837ea0b7a6d45fce23d5ae00a4
nazaninnasab <dranoer@gmail.com>
2023-07-09 08:18:11 +0200

    #1 Add screen

--- app/src/main/java/com/dranoer/gpt/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/dranoer/gpt/ui/screen/ChatScreen.kt
@@ -1,25 +1,56 @@
 package com.dranoer.gpt.ui.screen
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.dranoer.gpt.ui.component.ChatListLazyColumn
+import com.dranoer.gpt.ui.component.ChatScaffold
 import com.dranoer.gpt.ui.theme.GptTheme
+import com.dranoer.networking.model.ChatModel
+import com.dranoer.networking.model.UserType
 
 @Composable
-fun ChatScreen(modifier: Modifier = Modifier) {
+fun ChatScreen() {
 
-    Surface(modifier = modifier.fillMaxSize()) {
-        Text(text = "chat screen")
+    ChatScreenScaffold()
+}
+
+@Composable
+fun ChatScreenScaffold() {
+    ChatScaffold(
+        title = "GPTPlus",
+        userInputPromptText = "user said something..",
+    ) {
+        ChatListLazyColumn(
+            chatList = listOf(
+                ChatModel(
+                    text = "11111111111111111111111111111111111111",
+                    type = UserType.GPT
+                ),
+                ChatModel(
+                    text = "Helloooo",
+                    type = UserType.USER
+                ),
+                ChatModel(
+                    text = "how can i help you?",
+                    type = UserType.GPT
+                ),
+                ChatModel(
+                    text = "thius how thius how thius how thius how thius how thius how thius how thius how thius how thius how thius how thius how thius how thius how thius how ",
+                    type = UserType.USER
+                ),
+                ChatModel(
+                    text = "thius how thius how thius how ",
+                    type = UserType.GPT
+                )
+            )
+        )
     }
 }
 
 //region Preview
-@Preview("Normal detail screen")
+@Preview
 @Composable
-private fun ChatPreview_Normal() {
+private fun ChatScreenPreview_Normal() {
     GptTheme {
         ChatScreen()
     }
